2dac0e6c880041a4d075d9695bdb4efaff8a5ff1
rarukua <71946019+rarukua@users.noreply.github.com>
2026-01-23 18:51:35 +0000

    Deploying to gh-pages from @ rarukua/rarukua.github.io@4c1de4855ff5e3f5ff7491cad196a333ba8140b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
